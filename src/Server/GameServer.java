a9cf89253c891e95a46eb5170eb55f557ee7c301
IvanRadovan <ivano-radovanovic@hotmail.com>
2023-11-22 16:02:17 +0100

    Commit

--- src/Server/GameServer.java
+++ src/Server/GameServer.java
@@ -7,8 +7,6 @@ import java.util.List;
 
 public class GameServer {
     private static final int PORT = 9999;
-    private static List<ServerSidePlayer> players = new ArrayList<>();
-    private static int connectedPlayers = 0;
 
     public static void main(String[] args) throws IOException {
         ServerSocket listener = new ServerSocket();
